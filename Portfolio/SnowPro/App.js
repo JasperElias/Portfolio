2e79da54bb1e3753fd5c23fdd47214e81d866c5f
JasperElias <56274074+JasperElias@users.noreply.github.com>
2022-04-29 14:38:45 -0600

    Portfolio Uploaded
    Snowpro file upload

---
+++ Portfolio/SnowPro/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+//RootStack
+import RootStack from './navigators/RootStack';
+import dataNavigator from './navigators/dataNavigator'
+
+export default function App() {
+  return (
+    <RootStack/>
+    
+  );
+};
